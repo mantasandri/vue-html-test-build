0297000f5d4eee5782e733f82bf67f3d4d132dbf
Mantas A <mantas.andri@gmail.com>
2024-01-16 17:17:16 -0800

    rearrange email route

--- server/api/email.ts
+++ server/api/email.ts
@@ -2,30 +2,30 @@ import { useCompiler } from '#vue-email';
 
 export default defineEventHandler(async (event) => {
   const { username } = await readBody(event)
-
-  setTimeout(async () => {
-    try {
-      const template = await useCompiler('test.vue', {
-        props: {
-          username,
-        },
-      });
-
-      console.log(template)
-      
-      if (!template) {
-        throw createError({
-          statusCode: 404,
-          statusMessage: 'Not Found',
-        });
-      }
   
-      return template.html;
-    } catch (error) {
+  try {
+    setTimeout(() => {
+    }, 1000)
+    const template = await useCompiler('test.vue', {
+      props: {
+        username,
+      },
+    });
+
+    console.log(template)
+    
+    if (!template) {
       throw createError({
-        statusCode: 500,
-        statusMessage: 'Internal error',
+        statusCode: 404,
+        statusMessage: 'Not Found',
       });
     }
-  }, 1000)
+
+    return template.html;
+  } catch (error) {
+    throw createError({
+      statusCode: 500,
+      statusMessage: 'Internal error',
+    });
+  }
 });
