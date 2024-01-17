f1557205cb1d567cd26fc18daea67eded42532d1
Mantas A <mantas.andri@gmail.com>
2024-01-16 16:45:32 -0800

    swap app to pages and add index page

--- app.vue
+++ app.vue
@@ -1,22 +1,5 @@
-<script setup lang="ts">
-const email = ref('');
-
-const fetchAPI = async () => {
-  const apiRoute = '/api/email?username=JaneSmith'
-  console.log(apiRoute);
-  const result = await useFetch(apiRoute);
-  console.log(result);
-  email.value = result;
-};
-</script>
-
 <template>
-  <div>
-    <button @click.prevent="fetchAPI">Fetch Email</button>
-    <hr />
-    <div>
-      <h1>Email Output</h1>
-      <div v-html="email" />
-    </div>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const email = ref('');
+
+const fetchAPI = async () => {
+  const apiRoute = '/api/email?username=JaneSmith'
+  console.log(apiRoute);
+  const result = await useFetch(apiRoute);
+  console.log(result);
+  email.value = result;
+};
+</script>
+
+<template>
+  <div>
+    <button @click.prevent="fetchAPI">Fetch Email</button>
+    <hr />
+    <div>
+      <h1>Email Output</h1>
+      <div v-html="email" />
+    </div>
+  </div>
+</template>
